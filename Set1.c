a27bfaab653c4feaef8d305d52b18f7570621174
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-01-19 21:34:59 +0530

    Set1.c

---
+++ Set1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+Void main()
+{
+ int n;
+ Clrscr();
+ if(n>0)
+ {
+ Printf("positive");
+ }
+ else
+ if(n<0)
+ {
+ Printf("negative");
+ }
+ else
+ Printf("zero");
+ getch();
+ }
