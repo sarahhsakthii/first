4abf150b1d80d5891455dd75026be482ac965a31
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-01-19 22:09:15 +0530

    Oddeve.c

---
+++ Oddeve.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+Void main()
+{
+int n;
+clrscr();
+printf("enter num:");
+scanf("℅d",&n);
+if(n℅2==0)
+{
+printf("even");
+}
+else 
+{
+printf("odd");
+}
+getch();
+}
+
+
+
+
