f46e3402407f6d6d2baab00cc924f8415fd0bfa4
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-01-20 17:23:49 +0530

    Powexp.c

---
+++ Powexp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,res;
+clrscr();
+printf("enter the num:");
+scanf("%d%d",&a,&b);
+for(i=1;i<=b;i++)
+{
+res=res*a;
+}
+Printf("%d",res);
+getch();
+}
