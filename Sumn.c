2d0dada2821e4279b53353e84947855ccbbf8603
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-01-20 15:20:31 +0530

    Sumn.c

---
+++ Sumn.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,sum=0;
+clrscr();
+printf("enter the num:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+getch();
+}
