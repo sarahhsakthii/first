d2b852580d0a4ba3266f2370d39247ffa04b375e
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-01-20 14:05:04 +0530

    Vowelorcon.c

---
+++ Vowelorcon.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+clrscr();
+printf("enter the character:");
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+{
+printf("vowel");
+} 
+else
+{
+printf("consonant");
+}
+Getch();
+}
+
+
+
