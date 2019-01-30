c9531975851f91980d49a70b892d0a8985bf2547
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-01-30 22:59:30 +0530

    large32.c

---
+++ large32.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a[50],n,i,temp;
+ clrscr();
+ printf("enter num:");
+ scanf("%d",&n);
+ printf("enter ele:");
+ for(i=0;i<n;++i)
+ {
+ scanf("%d",&a[i]);
+ }
+ for(i=1;i<n;++i)
+ {
+  if(a[0]<a[i])
+  {
+  temp=a[0];
+  a[0]=a[i];
+  a[i]=temp;
+  }
+ }
+ printf("%d",a[0]);
+ getch();
+}
