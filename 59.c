de6fb1406e930756230df960d76c4f3cb4713868
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-02-04 22:56:21 +0530

    59.c

---
+++ 59.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a[15];
+ int n,c,r,i,sum=0;
+ clrscr();
+ printf("enter the num:");
+ scanf("%d",&n);
+ r=n%10;
+ c++;
+ a[i]=r;
+ i++;
+ for(i=c-1;i>=1;i--)
+ {
+ printf("%d",r);
+ }
+  getch();
+ }
