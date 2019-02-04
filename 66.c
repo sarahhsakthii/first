dd7e209a315bd682c4e0cd91dd28ee54f3811ecc
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-02-04 23:07:32 +0530

    66.c

---
+++ 66.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+ char a[50];
+ int i,c=0;
+ clrscr();
+ scanf("%s",a);
+ if(a[i]==0&&a[i]==9)
+ {
+ c++;
+ }
+ if(c==0)
+ {
+ printf("yes");
+ }
+ else
+ {
+ printf("no");
+ }
+ getch();
+ }
