09c6e45aa5830e4e2b1cde0e23fbd4dc7899fe43
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-02-12 20:03:49 +0530

    s79.c

---
+++ s79.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,m;
+ clrscr();
+ printf("enter num:");
+ scanf("%d%d",&n,&m);
+ c=n+m;
+ if(c%2==0)
+ {
+ printf("even");
+ }
+ else{
+ printf("odd");
+ }
+ getch();
+}
