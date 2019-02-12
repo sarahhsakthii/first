c22f5c7df910b241adb82ae0995d9c6dc979c8e2
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-02-12 20:02:01 +0530

    s78.c

---
+++ s78.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+        if(n%7==0)
+        {
+         printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+        
+
+
+    return 0;
+}
