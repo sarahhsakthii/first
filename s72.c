12f3623d80734c1a7a7bc62d6550e03288ab1227
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-02-12 18:59:33 +0530

    s72.c

---
+++ s72.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	char a[40];
+	int i,n,c=0;
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+	  if(a[i]=='0'||a[i]=='1')
+		{
+		    c=0;
+		 }
+		 else 
+		 {
+		     c++;
+		 }
+	}
+		 if(c==0)
+		 {
+		     printf("bin");
+		}
+		else
+		{
+		    printf("no");
+		}
+	return 0;
+}
