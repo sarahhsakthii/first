0232acc32193b81acf336600bd1e403c6a025894
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-02-17 23:40:56 +0530

    plr19.c

--- plr19.c
+++ plr19.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-  int n1,n2,i,j,flag=0,count=0;
+  int n1,n2,i,j,flag=0,c=0;
   scanf("%d %d",&n1,&n2);
   for(i=n1;i<=n2;i++)
   {
       flag=0;
-      for(j=2;j<=(i/2);j++)
+      for(j=2;j<=i/2;j++)
       {
           if((i%j)==0)
           {
@@ -20,9 +20,9 @@ int main()
       }
        if(flag==0)
         {
-            count++;
+            c++;
         }
   }
-  printf("%d",count);
+  printf("%d",c);
    return 0;
 }
