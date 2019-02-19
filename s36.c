9bae8c0302b4c2190e455b180122ec6d1f21dafe
sarahhsakthii <46845056+sarahhsakthii@users.noreply.github.com>
2019-02-19 21:32:50 +0530

    s36.c

---
+++ s36.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+ int a[50];
+ int n,i,j,t;
+ clrscr();
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+ for(j=i+1;j<n;j++)
+ {
+ if(a[i]>a[j])
+ {
+ t=a[i];
+ a[i]=a[j];
+ a[j]=t;
+ }
+ }
+ printf("%d ",a[i]);
+ }
+ getch();
+ }
